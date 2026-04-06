c0a032a0e2a9a47fc4ad38d95f0a3aabf6025d41
Jason Boutte <boutte3@llnl.gov>
2026-04-06 12:55:15 -0700

    test: import dramatiq redis broker

--- tests/test_stage2_imports.py
+++ tests/test_stage2_imports.py
@@ -2,5 +2,6 @@ def test_stage2_deps_import() -> None:
     import fastapi  # noqa: F401
     import httpx  # noqa: F401
     import dramatiq  # noqa: F401
+    from dramatiq.brokers.redis import RedisBroker  # noqa: F401
     import redis  # noqa: F401
     import uvicorn  # noqa: F401
